037e57167eb18107d362850a4196892b7bca4e8c
Adriano Schulter moenster <adrmoenster@gmail.com>
2022-02-24 08:10:37 -0300

    algumas partes exercicios

---
+++ exercicios/ex02/ex002.js
@@ -0,0 +1,8 @@
+function verificaidade(){
+    let dat = document.querySelector('input#txtdata');
+    let sex = document.get
+    let res = document.getElementById('res');
+
+    res.innerText = 'oi';
+    alert('oi')
+}
